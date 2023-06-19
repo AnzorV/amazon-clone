df1cd5fe526b41fb3460b8bbae7c6ed31754eaa5
Your Name <anzorv@gmail.com>
2023-06-19 20:56:44 +0100

    master

--- .history/src/Subtotal_20230619203819.js
+++ .history/src/Subtotal_20230619203819.js
@@ -3,7 +3,6 @@ import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
-import { getBasketTotal } from "./reducer";
 
 function Subtotal() {
     const [{ basket }, dispatch] = useStateValue();
@@ -22,7 +21,7 @@ function Subtotal() {
                 </>
             )}
             decimalScale={2}
-            value={getBasketTotal(basket)}
+            value={0}
             displayType={"text"}
             thousandSeperator={true}
             prefix={"$"}
